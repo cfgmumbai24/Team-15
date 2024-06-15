a5225d73a746ff49c4ef9849aa6799d4539a0ce0
Dipan-17 <93267025+Dipan-17@users.noreply.github.com>
2024-06-15 19:33:02 +0530

    SignIN layouts added

---
+++ app_code/JPMC_user_app/app/src/main/java/jpmc/team15/userapp/activities/SignInActivity.kt
@@ -0,0 +1,12 @@
+package jpmc.team15.userapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jpmc.team15.userapp.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
