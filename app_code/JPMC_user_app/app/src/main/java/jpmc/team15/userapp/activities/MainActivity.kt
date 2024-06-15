ce645f3b53c07fa1aff6e5a6d20a3866c0af5cf2
Dipan-17 <93267025+Dipan-17@users.noreply.github.com>
2024-06-15 19:32:50 +0530

    Layouts and defintions added

--- app_code/JPMC_user_app/app/src/main/java/jpmc/team15/userapp/activities/MainActivity.kt
+++ app_code/JPMC_user_app/app/src/main/java/jpmc/team15/userapp/activities/MainActivity.kt
@@ -1,12 +1,98 @@
 package jpmc.team15.userapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.graphics.toColorInt
+import androidx.core.view.GravityCompat
+import com.google.firebase.auth.FirebaseAuth
 import jpmc.team15.userapp.R
+import jpmc.team15.userapp.databinding.ActivityMainBinding
+import jpmc.team15.userapp.databinding.AppBarMainBinding
+import jpmc.team15.userapp.databinding.MainContentBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    private var mainBinding: ActivityMainBinding? = null //the parent layout
+    private var appBarBinding: AppBarMainBinding? = null //the top toolbar
+    private var mainContentBinding: MainContentBinding? = null //the recycler view containing
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainBinding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding?.root)
+
+        appBarBinding= AppBarMainBinding.bind(mainBinding?.drawerAppBar?.root!!)
+        mainContentBinding= MainContentBinding.bind(mainBinding?.drawerAppBar?.mainContent?.root!!)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setUpActionBar()
+
+        //side drawer ke buttons ke on click listeners
+        mainBinding?.navView?.setNavigationItemSelectedListener { menuItem->
+            when(menuItem.itemId){
+                R.id.nav_myProfile -> {
+                    Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_signOut -> {
+                    FirebaseAuth.getInstance().signOut()
+                    showErrorSnackBar("Successfully Signed out")
+                    val intent= Intent(this@MainActivity,IntroActivity::class.java)
+
+                    //if we had previous intro activity in stack use that one
+                    intent.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                    finish()
+                    true
+                }else ->false
+            }
+            mainBinding?.drawerLayout?.closeDrawer(GravityCompat.START)
+            return@setNavigationItemSelectedListener true
+        }
+
     }
+
+    private fun setUpActionBar(){
+
+        val toolbar=appBarBinding?.toolbarMainActivity
+        setSupportActionBar(toolbar)
+        actionBar?.title="Your Uploads"
+        toolbar?.title="Your Uploads"
+        toolbar?.setTitleTextColor("#ffffff".toColorInt())
+
+        toolbar?.setNavigationIcon(R.drawable.ic_navigation_menu)
+        toolbar?.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+
+    }
+
+    private fun toggleDrawer() {
+        if(mainBinding?.drawerLayout?.isDrawerOpen(GravityCompat.START) == true){
+            mainBinding?.drawerLayout?.closeDrawer(GravityCompat.START)
+        }else{
+            mainBinding?.drawerLayout?.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+
+        if(mainBinding?.drawerLayout?.isDrawerOpen(GravityCompat.START) == true){
+            mainBinding?.drawerLayout?.closeDrawer(GravityCompat.START)
+        }else{
+            doubleBackToExit()
+        }
+        super.onBackPressed()
+
+    }
+
+
 }
